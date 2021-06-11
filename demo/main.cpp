99fd771c7ba09c78e755e27f062431c7265953a7
107061105 <yc95086@gmail.com>
2021-06-11 09:19:44 -0700

    second

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include "math.h"
+#include "mbed.h"
 #include "bbcar.h"
 #include "bbcar_rpc.h"
 #include "string.h"
@@ -8,7 +9,7 @@ Ticker servo_ticker;
 PwmOut pin5(D5), pin6(D6);
 BufferedSerial pc(USBTX,USBRX); //tx,rx
 BufferedSerial uart(D1,D0); //tx,rx
-//DigitalInOut ping(D12);
+DigitalInOut ping(D12);
 BufferedSerial xbee(D10, D9);
 BBCar car(pin5, pin6, servo_ticker);
 
@@ -16,11 +17,13 @@ EventQueue queue(32 * EVENTS_EVENT_SIZE);
 Thread t1;
 Timer t;
 
-/*void ping_val(void) {
+int flag = 0;
+
+void ping_val(void) {
     float value;
-    char buffer[10];
-    xbee.set_baud(9600);
-    while(1) {
+    char buffer[20];
+    // xbee.set_baud(9600);
+    while(flag == 0) {
         ping.output();
         ping = 0; wait_us(200);
         ping = 1; wait_us(5);
@@ -32,7 +35,7 @@ Timer t;
         while(ping.read() == 1);
         value = t.read();
         value = value * 17700.4f;
-        sprintf(buffer, "%f\r\n", value);
+        sprintf(buffer, "Ping : %f\r\n", value);
         xbee.write(buffer, sizeof(buffer));
         //printf("%s\r\n", buffer);
         t.stop();
@@ -40,19 +43,24 @@ Timer t;
 
         ThisThread::sleep_for(1s);
     }
-}*/
+}
 
 int main(){
     uart.set_baud(9600);
     xbee.set_baud(9600);
 
-    //t1.start(callback(&queue, &EventQueue::dispatch_forever));
+    t1.start(callback(&queue, &EventQueue::dispatch_forever));
 
     xbee.set_blocking(false);
 
-    //xbee.sigio(mbed_event_queue()->event(ping_val));
+    xbee.sigio(mbed_event_queue()->event(ping_val));
+    
+    char buffer[20];
+    flag = 0; 
+    sprintf(buffer, "START TAG DETECT\r\n");
+    xbee.write(buffer, sizeof(buffer));
 
-    while(1){
+    while(flag == 0){
         if(uart.readable()) {
             double d1 = 0;
             double d2 = 0;
@@ -63,7 +71,6 @@ int main(){
             double degree = 0;
             double error_dx = 0;
             char buffer[20];
-
             char recv[1];
 
             uart.read(recv, sizeof(recv));
@@ -130,9 +137,6 @@ int main(){
                 degree = d1 + d2;
                 if (degree >= 270 && degree <= 360) degree = degree - 360;
 
-                sprintf(buffer, "%.2f, %.2f, %.2f\r\n", dx, dz, degree);
-                xbee.write(buffer, sizeof(buffer));
-
                 if (abs(dz) <= 4) {
                     if (abs(degree - 10 * dx) <= 8 && degree >= 0) {
                         car.stop();
@@ -160,6 +164,50 @@ int main(){
                     }
                 }
             }
+        }
+    }
+
+    flag = 1; 
+    sprintf(buffer, "START LINE DETECT\r\n");
+    xbee.write(buffer, sizeof(buffer));
+    ThisThread::sleep_for(2000ms);
+    car.follow(20, 20);
+
+    while(flag != 0){
+        if(uart.readable()) {
+            int value = 0;
+            int sign = 0;
+            char recv[1];
+        
+            uart.read(recv, sizeof(recv));
+            if (recv[0] == 's' && flag == 2) {
+                car.stop();
+                sprintf(buffer, "LINE DETECT OVER\r\n");
+                xbee.write(buffer, sizeof(buffer));
+                break;
+            } else if (recv[0] == 'f') {
+                car.follow(20, 20);
+            } else if (recv[0] == 'r') {
+                flag = 2;
+                do {
+                    uart.read(recv, sizeof(recv));
+                    if (recv[0] == '-') {
+                        sign = 1;
+                    } else if (recv[0] != '\r') {
+                        value = 10 * value + (((int)(recv[0])) - ((int)('0')));
+                    }
+                } while (recv[0] != '\r');
+
+                if (abs(value) > 10) {
+                    value = 10;
+                    if (sign) value = -10;
+                    car.follow(25.0 + value, -(25.0 - value));
+                } else {
+                    if (sign) value = value * -1;
+                    car.follow(35.0 + value, -35.0 + value);
+                }
+            }
+
         }
     }
 }
